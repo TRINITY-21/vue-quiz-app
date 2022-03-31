0aa28a681c35967f907061d61d1a08f66ccf8ad8
trinity <agyemanjoseph12@gmail.com>
2022-03-31 17:50:34 +0000

    init app

---
+++ src/components/Results.vue
@@ -0,0 +1,31 @@
+<template>
+
+ <div class="result">
+        <div class="title">{{results[resultsIndex].title}}</div>
+        <div class="desc">
+            {{results[resultsIndex].desc}}.
+        </div>
+    </div>
+
+</template>
+
+
+<script>
+
+export default {
+    props:["results", "totalCorrect"],
+
+    computed:{
+        resultsIndex(){
+            let index = 0;
+                this.results.forEach((e, i)=>{
+                    if(e.min <= this.totalCorrect && e.max >= this.totalCorrect){
+                        index = i
+                    }
+                })
+
+            return index;
+        }
+    }
+}
+</script>
---
+++ src/components/User.vue
@@ -0,0 +1,35 @@
+<script>
+
+export default {
+    data(){
+        return {
+            ages:"1"
+        }
+    },
+
+    props:{
+        age:{
+            type:[Number]
+        }
+    },
+
+    emits:["age-change"],
+
+    methods: {
+        updateAge(){
+            this.$emit("age-change", 2)
+        }
+    },
+}
+
+
+</script>
+
+
+<template>
+    <h1 v-if="age>30"> I am {{age}} years old</h1>
+
+    <h3 v-else>Hello age is changed {{age}} </h3>
+    <button @click="updateAge">update</button>
+
+</template>
